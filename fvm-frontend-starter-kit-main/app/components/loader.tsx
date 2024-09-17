dd59cdfaa3a243f56c61758b417721eee88ada84
Nikita Gugale <nikitagu1998@gmail.com>
2024-09-17 18:52:32 +0530

    "Fixing"

--- fvm-frontend-starter-kit-main/app/components/loader.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="flex flex-row gap-2">
-            <div className="w-4 h-4 rounded-full bg-yellow-300 animate-bounce [animation-delay:.7s]"></div>
-            <div className="w-4 h-4 rounded-full bg-yellow-300 animate-bounce [animation-delay:.3s]"></div>
-            <div className="w-4 h-4 rounded-full bg-yellow-300 animate-bounce [animation-delay:.7s]"></div>
-        </div>
-    );
-}
